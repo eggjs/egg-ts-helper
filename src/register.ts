d80f05729a13edb21ab4c46bef0784aa8f26d486
wanghx <whxaxes@gmail.com>
2018-03-27 19:06:25 +0800

    fix: fixed register error

--- src/register.ts
+++ src/register.ts
@@ -4,26 +4,24 @@ import * as path from 'path';
 import { createTsHelperInstance } from './';
 
 // only works in master
-if (cluster.isWorker) {
-  process.exit(0);
+if (cluster.isMaster) {
+  // fork a process to watch files change
+  const ps = fork(path.resolve(__dirname, './bin'), ['-w'], {
+    execArgv: [],
+  });
+
+  // kill child process while process exit
+  function close() {
+    if (!ps.killed) {
+      ps.kill('SIGHUP');
+    }
+  }
+
+  process.on('exit', close);
+  process.on('SIGINT', close);
+  process.on('SIGTERM', close);
+  process.on('SIGHUP', close);
+
+  // exec building at first
+  createTsHelperInstance().build();
 }
-
-// fork a process to watch files change
-const ps = fork(path.resolve(__dirname, './bin'), ['-w'], {
-  execArgv: [],
-});
-
-// kill child process while process exit
-function close() {
- if (!ps.killed) {
-   ps.kill('SIGHUP');
- }
-}
-
-process.on('exit', close);
-process.on('SIGINT', close);
-process.on('SIGTERM', close);
-process.on('SIGHUP', close);
-
-// exec building at first
-createTsHelperInstance().build();
