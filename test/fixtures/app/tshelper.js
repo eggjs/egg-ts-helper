2de57cfe1bf26d5b02e8ddff638e5806bd6184a6
plusmancn <plusmancn@gmail.com>
2018-11-12 15:50:54 +0800

    fix: caseStyle return string type

--- test/fixtures/app/tshelper.js
+++ test/fixtures/app/tshelper.js
@@ -18,7 +18,7 @@ module.exports = {
       pattern: '**/*.schema.(ts|js)',
       caseStyle: filename => {
         const p1 = filename.split('.schema')[0];
-        return [ p1.replace(/[_-][a-z]/ig, s => s.substring(1).toUpperCase()) ];
+        return p1.replace(/[_-][a-z]/ig, s => s.substring(1).toUpperCase());
       }
     }
   }
