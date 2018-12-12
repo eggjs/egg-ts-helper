c0b6fadaf38db167c573ffa47e76cc4450c82ffb
wanghx <whxaxes@gmail.com>
2018-12-12 20:47:58 +0800

    f

--- src/generators/function.ts
+++ src/generators/function.ts
@@ -2,6 +2,6 @@ import { TsGenConfig, TsHelperConfig } from '..';
 import classGen from './class';
 
 export default function(config: TsGenConfig, baseConfig: TsHelperConfig) {
-  config.interfaceHandle = config.interfaceHandle || 'ReturnType<typeof {{0}}>';
+  config.interfaceHandle = config.interfaceHandle || 'ReturnType<typeof {{ 0 }}>';
   return classGen(config, baseConfig);
 }
