2fece2333b96faf2f4360031fcbffc52bc74d0c8
wanghx <whxaxes@gmail.com>
2019-02-24 13:22:50 +0800

    fix: register should use env instead of file

--- test/register.test.ts
+++ test/register.test.ts
@@ -21,30 +21,7 @@ const runRegister = (opt?: Partial<typeof options>) => {
 
 describe('register.test.ts', () => {
   beforeEach(() => {
-    del.sync(path.resolve(__dirname, '../.cache'));
     del.sync(path.resolve(__dirname, './fixtures/app8/typings'), { force: true });
-  });
-
-  it('should cache pid', async () => {
-    const ps = runRegister();
-    const { stdout, stderr } = await getStd(ps);
-
-    assert(!stderr);
-    assert(stdout.includes('create'));
-    assert(fs.existsSync(path.resolve(__dirname, '../.cache')));
-    const pid = fs.readFileSync(path.resolve(__dirname, '../.cache')).toString();
-
-    const ps2 = runRegister();
-    const { stdout: stdout2 } = await getStd(ps2);
-    assert(!stdout2.includes('create'));
-    assert(pid === fs.readFileSync(path.resolve(__dirname, '../.cache')).toString());
-  });
-
-  it('should works while cache pid is not exist', async () => {
-    const pid = '23567';
-    fs.writeFileSync(path.resolve(__dirname, '../.cache'), pid);
-    await getStd(runRegister(), true);
-    assert(pid !== fs.readFileSync(path.resolve(__dirname, '../.cache')).toString());
   });
 
   it('should works with --require without error', async () => {
