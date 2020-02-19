d83471fe6032d3f12476d107573c31674bca35da
赵长伟 <changwei.zcw@alibaba-inc.com>
2020-02-19 19:23:54 +0800

    feat: hack support for webstorm(intellij)

--- src/generators/egg.ts
+++ src/generators/egg.ts
@@ -8,7 +8,14 @@ export default function(config: TsGenConfig, baseConfig: TsHelperConfig) {
   return {
     dist: path.resolve(config.dtsDir, 'index.d.ts'),
     content:
+      `import { Context, IService } from '${baseConfig.framework}';\n` +
       `export * from '${baseConfig.framework}';\n` +
-      'export as namespace Egg;\n',
+      'export as namespace Egg;\n\n' +
+      `\/\/ hack support for webstorm (intellij)
+declare module 'egg' {
+  export interface Context {
+    service: IService;
+  }
+}`,
   };
 }
