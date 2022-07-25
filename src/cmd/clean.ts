f02161ac00769e85e444d4ddc08bcc7b936f0bae
wanghx <whx89768@antgroup.com>
2022-07-25 17:24:17 +0800

    feat: deprecated clean command

--- src/cmd/clean.ts
+++ src/cmd/clean.ts
@@ -1,10 +1,12 @@
 import { cleanJs } from '../utils';
+import chalk from 'chalk';
 
 class CleanCommand implements SubCommand {
   description = 'Clean js file while it has the same name ts/tsx file';
 
   async run(_, { cwd }: SubCommandOption) {
     cleanJs(cwd);
+    console.info(chalk.red('\nWARNING: `ets clean` has been deprecated! Use `tsc -b -c` instead\n'));
   }
 }
 
