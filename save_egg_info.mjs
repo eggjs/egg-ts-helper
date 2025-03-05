2a63587a59000a3a7eebf271e448694514a6cd39
fengmk2 <fengmk2@gmail.com>
2025-03-05 23:44:44 +0800

    fix: make sure parent dir exists

--- save_egg_info.mjs
+++ save_egg_info.mjs
@@ -37,6 +37,7 @@ async function main() {
   eggInfo.eggPaths = loader.eggPaths;
   eggInfo.timing = Date.now() - startTime;
   console.warn('[egg-ts-helper] plugins: %j', Object.keys(eggInfo.plugins));
+  await fs.mkdir(path.dirname(saveEggInfoPath), { recursive: true });
   await fs.writeFile(saveEggInfoPath, JSON.stringify(eggInfo, null, 2));
 }
 
